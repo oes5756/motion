4d5e01d8b98433004b795085346976fddc0cc531
kanghyeongu <kanghg@synustech.com>
2023-09-04 15:53:45 +0900

    hello

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class pasdfsadfssadfads
+{
+    static void Main()
+    {
+        Console.WriteLine("구구단을 출력할 숫자를 입력하세요: ");
+        int num = int.Parse(Console.ReadLine());
+
+        Console.WriteLine($"{num}단을 출력합니다:");
+
+        for (int i = 1; i <= 9; i++)
+        {
+            Console.WriteLine($"{num} x {i} = {num * i}");
+        }
+    }
+}
